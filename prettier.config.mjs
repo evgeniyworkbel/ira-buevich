a6057892f561a2f340e02d4fdbc56431ec5af28c
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-09-01 23:14:43 +0300

    fix: correct path for tw plugin sorter, add jsdoc typing to appear IDE autocomplete

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,10 @@
 import myCustomConfig from "@evgeniyworkbel/prettier-config";
 
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   ...myCustomConfig,
   plugins: ["prettier-plugin-tailwindcss"],
 
   // Settings provided for prettier-plugin-tailwindcss
-  tailwindStylesheet: "./src/app/global.css",
+  tailwindStylesheet: "./src/app/globals.css",
 };
