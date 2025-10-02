ecea757a4bd73985d2803a8d7fc348e00fecabec
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-02 22:03:00 +0300

    feat: set default type button, set cursor pointer by default for button

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,15 +1,16 @@
-import { PropsWithChildren } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { cn } from "../lib";
 
-type ButtonProps = PropsWithChildren<{ className?: string }>;
+type ButtonProps = Pick<ButtonHTMLAttributes<HTMLButtonElement>, "className" | "type" | "children">;
 
-export function Button({ className, children }: ButtonProps) {
+export function Button({ className, type = "button", children }: ButtonProps) {
   return (
     <button
       className={cn(
-        "inline-flex h-fit w-fit shrink-0 items-center justify-center rounded-[40px] px-3 py-4 text-sm font-semibold text-foreground-surface",
+        "inline-flex h-fit w-fit shrink-0 cursor-pointer items-center justify-center rounded-[40px] px-3 py-4 text-sm font-semibold text-foreground-surface",
         className,
       )}
+      type={type}
     >
       {children}
     </button>
