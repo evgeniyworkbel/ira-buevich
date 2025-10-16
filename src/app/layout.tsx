6303f03847377fe6637381e0cce42d50526686f1
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-16 20:33:50 +0300

    fix: repair script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YM_COUNTER_ID}ss', 'ym');
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YM_COUNTER_ID}', 'ym');
 
-    ym(${YM_COUNTER_ID}ss, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
+    ym(${YM_COUNTER_ID}, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
           }}
         ></script>
       </head>
