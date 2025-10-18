e66233a57ee416b819bc756eb233a876eea9eaf9
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-19 00:15:31 +0300

    refactor: add todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import {
 export default function Page() {
   return (
     // todo: сделать smooth скролл
+    // todo: добавить Отзывы и кнопку Наверх
     <div className="min-h-screen max-w-[1920px] bg-background text-foreground">
       <Header />
       <main>
