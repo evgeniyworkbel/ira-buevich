5a32b96ad949d42f52263d83569b89af7a358c58
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-21 14:43:30 +0300

    feat: replace paragraph with div for content container to avoid hydration error
    Source: https://nextjs.org/docs/messages/react-hydration-error#common-causes

--- src/shared/ui/accordion/accordion-item.tsx
+++ src/shared/ui/accordion/accordion-item.tsx
@@ -2,7 +2,7 @@ import { AccordionArrow } from "./accordion-arrow";
 
 export type AccordionItemProps = {
   title: string;
-  content: string;
+  content: React.ReactNode;
 };
 
 export function AccordionItem({ title, content }: AccordionItemProps) {
@@ -13,9 +13,9 @@ export function AccordionItem({ title, content }: AccordionItemProps) {
         <AccordionArrow className="group-has-checked:translate-y-1/4 group-has-checked:rotate-45" />
         <input type="checkbox" hidden />
       </label>
-      <p className="h-0 overflow-clip px-6 transition-all group-has-checked:h-auto group-has-checked:pt-0 group-has-checked:pb-6">
+      <div className="h-0 overflow-clip px-6 transition-all group-has-checked:h-auto group-has-checked:pt-0 group-has-checked:pb-6">
         {content}
-      </p>
+      </div>
     </div>
   );
 }
